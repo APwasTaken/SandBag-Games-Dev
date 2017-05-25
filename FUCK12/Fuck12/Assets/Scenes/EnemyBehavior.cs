bcd87efeca4eef883ab5848c3a16a1dfcefa0bd2
Guambo <nvallejos1234@gmail.com>
2017-05-24 20:45:18 -0400

    Auto stash before merge of "master" and "origin/master"

--- FUCK12/Fuck12/Assets/Scenes/EnemyBehavior.cs
+++ FUCK12/Fuck12/Assets/Scenes/EnemyBehavior.cs
@@ -3,23 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour {
-	Rigidbody2D rigBod;
-	public float hp = 0.0f;
+
+	Rigidbody2D rigBod;                               // Used later in the Start() function
+	public float hp = 0.0f;	                          // This allows you to set the hp of the enemy in Unity
+
 	// This handles player collision
 	void OnTriggerEnter2D(Collider2D other) {
-			  // If enemy collides with the player, subtract 1 from player hp
-				// as well as destroy this enemy game object
         if (other.CompareTag("Player")) {
-            Player.hp -= 1;
-						Destroy(gameObject);
-						// Debug.Log(Player.hp, this);
+            Player.hp -= 1;                        // If enemy collides with the player, subtract 1 from player hp
+						Destroy(gameObject);  								 // as well as destroy this enemy game object
+						//Debug.Log(Player.hp, this);            // This shows that the players hp is being changed when colliding with enemy object
 				}
   }
-	// Enemy starts with 2 hp
+
+	// Initialize Enemy properties
 	void Start () {
-		hp = 2;
-		rigBod = GetComponent<Rigidbody2D>();
-		rigBod.velocity = new Vector2(-3,0);
+		hp = 2;                                         // Enemy starts with 2 hp
+		rigBod = GetComponent<Rigidbody2D>();		        // Initialize(sort of) the enemy to a Rigidbody2D object
+		rigBod.velocity = new Vector2(-3,0);					  // Enemy moves in the negative x-axis direction, we will change to negative y-axis direction
 	}
 
 	// Update is called once per frame
